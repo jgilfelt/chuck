4bf6aab048d9e3109d3bbac2f8dfa4b99bdbf736
Jeff Gilfelt <jgilfelt@gmail.com>
2017-02-04 13:55:01 +0000

    consolodate theme

--- library/src/main/java/com/github/jgilfelt/chuck/ui/MainActivity.java
+++ library/src/main/java/com/github/jgilfelt/chuck/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.github.jgilfelt.chuck.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.github.jgilfelt.chuck.R;
 import com.github.jgilfelt.chuck.data.HttpTransaction;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements TransactionListFr
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chuck_activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, TransactionListFragment.newInstance())
