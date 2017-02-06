36d95ba21ca16b1f9cb52e1b9e7dfd37fbefd31a
Jeff Gilfelt <jgilfelt@gmail.com>
2017-02-06 00:13:10 +0000

    add license headers

--- library/src/main/java/com/readystatesoftware/chuck/internal/support/DividerItemDecoration.java
+++ library/src/main/java/com/readystatesoftware/chuck/internal/support/DividerItemDecoration.java
@@ -1,5 +1,3 @@
-package com.readystatesoftware.chuck.internal.support;
-
 /*
  * Copyright (C) 2014 The Android Open Source Project
  *
@@ -15,6 +13,7 @@ package com.readystatesoftware.chuck.internal.support;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.readystatesoftware.chuck.internal.support;
 
 import android.content.Context;
 import android.content.res.TypedArray;
